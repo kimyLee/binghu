b2a828488f11d8f6e809408f2df997aa1e0defe8
kimylee <1095643875@qq.com>
2017-12-08 09:12:39 +0800

    12-8

--- src/components/powerLine.vue
+++ src/components/powerLine.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="powerline">
-    <button @click="status = status ? 0 : 1" class="shotBtn">shot</button>
+    <button @click="slowPowerDecrease" class="testBtn">click</button>
+    <button @click="decreasePower" class="shotBtn">shot</button>
+    <button @click="switchOption" class="stopBtn">stop/start</button>
     <svg :width="clientWidth/2" :height="clientWidth/2">
     <circle :cx="clientWidth/4" :cy="clientWidth/2" :r="clientWidth/4 - 20" stroke-width="15" stroke="#ccc" fill="none"></circle>
     <circle :cx="clientWidth/4" :cy="clientWidth/2" :r="clientWidth/4 - 20" stroke-width="15" stroke="#444" fill="none"  :stroke-dasharray="circleDasharray"></circle>
@@ -15,10 +17,14 @@ export default {
   data () {
     return {
       status: 0,                  // 进度条状态， 0：来回变化， 1：衰减， 2：停止
+      beforeStatus: 0,            // 暂停之前状态
       clientWidth: 0,             // 屏幕宽度
       progress: 0,                // 力度， 0-100
       PowerIncrease: 1,           // 力度增强还是衰弱
-      PowerIncreaseSpeed: 1,      // 力度增减速率
+      PowerIncreaseSpeed: 1,      // 力度摇摆增减速率
+      PowerDecreaseTime: 50,      // 力度衰减时间间隔
+      decreaseTimer: '',              // 力度衰减重复进行计时器
+      slowTimer: '',              // 力度衰减减缓计时器
       PowerIncreaseFactor: 0      // 力度和stroke-dasharray计算因子
     }
   },
@@ -41,6 +47,7 @@ export default {
     init () {
       this.swingPower()
     },
+    // 摇摆选择力度
     swingPower () {
       this.progress = this.progress + this.PowerIncrease * this.PowerIncreaseSpeed
       if (this.progress >= 100) {
@@ -53,6 +60,50 @@ export default {
       if (this.status === 0) {
         window.requestAnimationFrame(this.swingPower)
       }
+    },
+    // 力度衰减
+    decreasePower () {
+      this.status = 1
+      clearTimeout(this.decreaseTimer)
+      this.progress = this.progress - 0.5
+      if (this.progress < 0) {
+        this.gameOver()
+        return
+      }
+      this.decreaseTimer = setTimeout(() => {
+        this.decreasePower()
+      }, this.PowerDecreaseTime);
+    },
+    // 力度减缓
+    slowPowerDecrease () {
+      clearTimeout(this.slowTimer)
+      this.PowerDecreaseTime = 100
+      this.slowTimer = setTimeout(() => {
+        this.PowerDecreaseTime = 50
+      }, 200);
+    },
+     // 暂停、恢复
+    switchOption () {
+      if (this.status === 2) {
+        this.status = this.beforeStatus
+        return this.beforeStatus === 0 ? this.swingPower() : ''
+      }
+      this.beforeStatus = this.status
+      this.status = 2
+    },
+    // 重置复位
+    reset () {
+      this.status = 0
+      this.beforeStatus = 0
+      this.progress = 0
+      this.PowerIncrease = 1
+      this.PowerIncreaseSpeed = 1
+      this.PowerDecreaseTime = 50
+    },
+    gameOver () {
+      alert('over')
+      this.reset()
+      this.init()
     }
   }
 }
@@ -73,6 +124,18 @@ export default {
       position: absolute;
       bottom: 0;
       left: 30px;
+    }
+    .stopBtn {
+      position: absolute;
+      bottom: 0;
+      left: 60px;
+    }
+    .testBtn {
+      position: fixed;
+      bottom: 30px;
+      width: 60px;
+      height: 30px;
+      left: 60px;
     }
     
   }
